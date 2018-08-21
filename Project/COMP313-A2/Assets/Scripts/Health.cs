c4698c0d9c42e1386cbf0e588ad2cf085fec2d84
Finn Welsford-Ackroyd <welsfofinn@ecs.vuw.ac.nz>
2018-08-22 04:29:21 +1200

    Enemies can now damage the player (Closes #8)

---
+++ Project/COMP313-A2/Assets/Scripts/Health.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Health : MonoBehaviour
+{    float health;
+    public UnityEvent onDeath;
+
+    // Use this for initialization
+    void Start()
+    {
+        health = 100;
+    }
+
+    public void Damage(float amount)
+    {
+        health -= amount;
+		print("Player health = "+health);
+        if (health <= 0)
+        {
+            onDeath.Invoke();
+        }
+    }
+}
