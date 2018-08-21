a7296132cdf0b637fe72290d94ddfc5e2f66cb11
Finn Welsford-Ackroyd <welsfofinn@ecs.vuw.ac.nz>
2018-08-21 19:10:24 +1200

    Organised project files and imported scripts

---
+++ Project/COMP313-A2/Assets/Scripts/EnemyTasks.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using Panda;
+
+public class EnemyTasks : MonoBehaviour
+{
+    public bool paused = true;
+    public Transform player;
+    public float runSpeed = 10;
+    public Transform[] patrolPoints;
+    NavMeshAgent agent;
+    Transform agentTransform;
+    Animator animator;
+    float requiredProximity = 0.25f;
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        agent.updatePosition = true;
+        agent.updateRotation = true;
+        agent.speed = runSpeed;
+        agentTransform = GetComponent<Transform>();
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        animator.SetFloat("Forward", agent.desiredVelocity.magnitude, 0.1f, Time.deltaTime);
+    }
+
+    [Task]
+    bool Paused()
+    {
+        return paused;
+    }
+
+    /*
+     * Move to the destination
+     */
+    [Task]
+    void MoveToPatrolPoint(int pointIndex)
+    {
+        if (agent != null)
+        {
+            agent.SetDestination(patrolPoints[pointIndex].position);
+            WaitArrival();
+        }
+    }
+
+    [Task]
+    public void WaitArrival()
+    {
+        var task = Task.current;
+        float d = agent.remainingDistance;
+        if (agent.velocity.magnitude < 2f && d < 4.5f && !task.isStarting)
+        {
+            task.Fail();
+        }
+        if (!task.isStarting && d < requiredProximity)
+        {
+            task.Succeed();
+            d = 0.0f;
+        }
+
+        if (Task.isInspected)
+            task.debugInfo = string.Format("d-{0:0.00}", d);
+    }
+
+    [Task]
+    void Die()
+    {
+        agent.isStopped = true;
+        animator.SetTrigger("DeathTrigger");
+        Task.current.Succeed();
+    }
+}
---
+++ Project/COMP313-A2/Assets/Scripts/PlayerDeath.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class PlayerDeath : MonoBehaviour
+{
+    Animator animator;
+    // Use this for initialization
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    public void Die()
+    {
+        animator.SetTrigger("DeathTrigger");
+    }
+}
