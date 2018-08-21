6462bbfdcd222cfc2c23d16f4d0862c69a8380ed
Finn Welsford-Ackroyd <welsfofinn@ecs.vuw.ac.nz>
2018-08-22 05:30:57 +1200

    Player can now disable enemies from behind (closes #11)
    Need to copy script across to all enemies

---
+++ Project/COMP313-A2/Assets/OnClick.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class OnClick : MonoBehaviour
+{
+
+	public float maxDistance;
+	public Transform player;
+	public UnityEvent click;
+	Transform button;
+
+    void Start()
+    {
+        button = GetComponent<Transform>();
+    }
+
+    void OnMouseDown()
+    {
+		if(Vector3.Distance(button.position, player.position) <= maxDistance)
+		{
+			click.Invoke();
+		}
+    }
+}
